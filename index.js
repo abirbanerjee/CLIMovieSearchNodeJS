0acbc0634a75be7653eceb31a0558db20a1d3fd3
Abir Banerjee <abir.banerjee@gmail.com>
2022-07-17 22:28:44 +0200

    change of font and colots

--- index.js
+++ index.js
@@ -13,28 +13,31 @@ const URI = 'http://www.omdbapi.com/?apikey=94dda6e6';
 cfonts.say('Search movie/game info',{
     font:'shade',
     align:'center',
-    lineheight:1
+    lineheight:1,
+    gradient:['yellow','red']
 });
 const menu = async()=>{
 cfonts.say('Enter your choice:',{font:'chrome',
 align:'center',
-color:'cyanBright.'});
+colors:['cyan']});
 cfonts.say(`1. Search by title.
 2. Search by valid IMDB id.
 3. Exit`,{
     font:'chrome',
     align:'center',
-    color:'cyanBright.'
+    colors:['yellowBright']
 });
 
 rl.question('>',(ans)=>{
     switch(ans){
         case '1':
-            cfonts.say('Enter movie name:',{font:'chrome',
-            align:'center',})
+            cfonts.say('Enter movie name:',{font:'console',
+            align:'center',
+            colors:['blue']})
             rl.question('>', async(movName)=>{
-                cfonts.say('Enter year of release if known(press enter to leave blank):',{font:'chrome',
-                align:'center',})
+                cfonts.say('Enter year of release if known(press enter to leave blank):',{font:'console',
+                align:'center',
+            colors:['yellow']})
                 rl.question('>',async (movYear)=>{
                     if(movYear==='')
                     {
@@ -46,7 +49,9 @@ rl.question('>',(ans)=>{
                     }
                     const reply = await axios.get(`${searchString}`);
                 if(reply.data.Response==='False'){
-                    console.log('No/too many results. Please try again.');
+                    cfonts.say('No/too many results. Please try again.', {font:'console',
+                    align:'center',
+                colors:['red']});
                     menu();
                 }
                 else{
@@ -55,9 +60,9 @@ rl.question('>',(ans)=>{
                 })
                 
                 // console.log(reply.data.Search[0]);
-                cfonts.say('Your search returned the following results:',{font:'chrome',
+                cfonts.say('Your search returned the following results:',{font:'console',
                 align:'center',
-                color:'cyanBright.'});
+                colors:['cyanBright']});
                 console.table(reply.data.Search);
                 rl.question(`Enter index to see details or "x" to exit:`,async(movIndex)=>{
                     if(movIndex==='x')
@@ -68,32 +73,34 @@ rl.question('>',(ans)=>{
                     srchType = reply.data.Search[parseInt(movIndex)].Type;
                     const title = reply.data.Search[parseInt(movIndex)].Title;
                     const yearOfRelease = reply.data.Search[parseInt(movIndex)].Year;
-                    cfonts.say(`You chose ${title}`,{font:'chrome',
+                    cfonts.say(`You chose ${title}`,{font:'console',
                     align:'center',
-                    color:'cyanBright.'});
+                    color:['cyanBright'],
+                    lineheight:1});
                     const movDets = await axios.get(encodeURI(`${URI}&t=${title}&y=${yearOfRelease}`));
                     // console.log(movDets.data);
-                    cfonts.say(`${movDets.data.Title} is a ${srchType} of ${movDets.data.Genre} genre directed by ${movDets.data.Director} and written by ${movDets.data.Writer}. The ${srchType} starred ${movDets.data.Actors} in lead roles and released on ${movDets.data.Released}. `,{font:'chrome',
+                    cfonts.say(`${movDets.data.Title} is a ${srchType} of ${movDets.data.Genre} genre directed by ${movDets.data.Director} and written by ${movDets.data.Writer}. The ${srchType} starred ${movDets.data.Actors} in lead roles and released on ${movDets.data.Released}. `,{font:'console',
                     align:'center',
-                    color:'cyanBright.'});
+                    colors:['whiteBright'],
+                    lineheight:1});
                     if(movDets.data.Plot != 'N/A'){
-                        cfonts.say(`The ${srchType} has the following plot: ${movDets.data.Plot}`,{font:'chrome',
+                        cfonts.say(`The ${srchType} has the following plot: ${movDets.data.Plot}`,{font:'console',
                         align:'center',
-                        color:'cyanBright.'})
+                        colors:['whiteBright']})
                     }
                     if(movDets.data.Awards != 'N/A'){
-                        cfonts.say(`The ${srchType} has ${movDets.data.Awards}.`, {font:'chrome',
+                        cfonts.say(`The ${srchType} has ${movDets.data.Awards}.`, {font:'console',
                         align:'center',
-                        color:'cyanBright.'})
+                        colors:['whiteBright']})
                     }
                     if(movDets.data.Ratings.length>0){
-                        cfonts.say(`The ${srchType} has the following ratings:`,{font:'chrome',
+                        cfonts.say(`The ${srchType} has the following ratings:`,{font:'console',
                         align:'center',
-                        color:'cyanBright.'});
+                        colors:['whiteBright']});
                         movDets.data.Ratings.forEach(rating=>{
-                            cfonts.say(`${rating.Value} by ${rating.Source}.`,{font:'chrome',
+                            cfonts.say(`${rating.Value} by ${rating.Source}.`,{font:'console',
                             align:'center',
-                            color:'cyanBright.'});
+                            colors:['whiteBright']});
                         })
                     }
 
@@ -106,33 +113,33 @@ rl.question('>',(ans)=>{
             break;
 
         case '2':
-            cfonts.say('Enter valid imdb id:', {font:'chrome',
+            cfonts.say('Enter valid imdb id:', {font:'console',
             align:'center',
             color:'cyanBright'})
             rl.question('>',async(imdbId)=>{
                 searchString = `${URI}&i=${imdbId}`;
                 const movieDetails = await axios.get(`${searchString}`);
-                cfonts.say(`${movieDetails.data.Title} is a ${movieDetails.data.Type} of ${movieDetails.data.Genre} genre directed by ${movieDetails.data.Director} and written by ${movieDetails.data.Writer}. The ${movieDetails.data.Type} starred ${movieDetails.data.Actors} in lead roles and released on ${movieDetails.data.Released}. `,{font:'chrome',
+                cfonts.say(`${movieDetails.data.Title} is a ${movieDetails.data.Type} of ${movieDetails.data.Genre} genre directed by ${movieDetails.data.Director} and written by ${movieDetails.data.Writer}. The ${movieDetails.data.Type} starred ${movieDetails.data.Actors} in lead roles and released on ${movieDetails.data.Released}. `,{font:'console',
                 align:'center',
-                color:'cyanBright.'});
+                colors:['whiteBright']});
                     if(movieDetails.data.Plot != 'N/A'){
-                        cfonts.say(`The ${movieDetails.data.Type} has the following plot: ${movieDetails.data.Plot}`, {font:'chrome',
+                        cfonts.say(`The ${movieDetails.data.Type} has the following plot: ${movieDetails.data.Plot}`, {font:'console',
                         align:'center',
-                        color:'cyanBright.'})
+                        colors:['whiteBright']})
                     }
                     if(movieDetails.data.Awards != 'N/A'){
-                        cfonts.say(`The ${movieDetails.data.Type} has ${movieDetails.data.Awards}.`,{font:'chrome',
+                        cfonts.say(`The ${movieDetails.data.Type} has ${movieDetails.data.Awards}.`,{font:'console',
                         align:'center',
-                        color:'cyanBright.'})
+                        colors:['whiteBright']})
                     }
                     if(movieDetails.data.Ratings.length>0){
-                        cfonts.say(`The ${movieDetails.data.Type} has the following rating(s):`,{font:'chrome',
+                        cfonts.say(`The ${movieDetails.data.Type} has the following rating(s):`,{font:'console',
                         align:'center',
-                        color:'cyanBright.'});
+                        colors:['whiteBright']});
                         movieDetails.data.Ratings.forEach(rating=>{
-                            cfonts.say(`${rating.Value} by ${rating.Source}.`, {font:'chrome',
+                            cfonts.say(`${rating.Value} by ${rating.Source}.`, {font:'console',
                             align:'center',
-                            color:'cyanBright.'});
+                            colors:['whiteBright']});
                         })
                     }
                 menu();
@@ -142,9 +149,9 @@ rl.question('>',(ans)=>{
             rl.close();
             break;
         default:
-            cfonts.say('Invalid choice. Please try again.',{font:'chrome',
+            cfonts.say('Invalid choice. Please try again.',{font:'console',
             align:'center',
-            color:'cyanBright.'});
+            colors:['whiteBright']});
             menu();
     }
 })
